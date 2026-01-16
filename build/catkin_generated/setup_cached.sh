090309ce7e17419c10f246f63bdcc004a0c721f0
wwz <w2061672219@gmail.com>
2026-01-16 10:30:14 +0800

    1.16.1init

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/wwz/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/home/nvidia/cv_bridge_ws/devel/lib:/root/catkin_ws/devel/lib:/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu:/lib:/usr/local/cuda-11.5/lib64:/usr/local/cuda-11.5/lib64:/usr/local/cuda-11.5/lib64:/home/nvidia/TensorRT-8.6.1.6/lib:/usr/local/openssl/lib64'
+export PATH='/opt/ros/noetic/bin:/usr/local/openssl/bin:/root/.vscode-server/data/User/globalStorage/github.copilot-chat/debugCommand:/root/.vscode-server/data/User/globalStorage/github.copilot-chat/copilotCli:/root/.vscode-server/cli/servers/Stable-94e8ae2b28cb5cc932b86e1070569c4463565c37/server/bin/remote-cli:/usr/local/openssl/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/local/cuda-11.5/bin:/root/.vscode-server/extensions/ms-python.debugpy-2025.18.0-linux-x64/bundled/scripts/noConfigScripts:/usr/local/cuda-11.5/bin'
+export PWD='/wwz/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/wwz/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/wwz/src:$ROS_PACKAGE_PATH"
